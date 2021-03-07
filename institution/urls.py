41ec0d24d97db69c56796cc35d19123994b901f7
Laban <labankiplagat81@gmail.com>
2021-03-08 01:20:23 +0300

    Update urls to accept uuid kwarg

--- institution/urls.py
+++ institution/urls.py
@@ -15,11 +15,11 @@ from .views import (
 urlpatterns = [
     path('', Homepage, name='home'),
     path('add-ambulance/', AmbulanceCreateView.as_view(), name='create-ambulance'),
-    path('update-ambulance/<int:pk>/', AmbulanceUpdateView.as_view(), name='update-ambulance'),
-    path('delete-ambulance/<int:pk>/', AmbulanceDeleteView.as_view(), name='delete_ambulance'),
+    path('update-ambulance/<uuid:pk>/', AmbulanceUpdateView.as_view(), name='update-ambulance'),
+    path('delete-ambulance/<uuid:pk>/', AmbulanceDeleteView.as_view(), name='delete_ambulance'),
     path('create-institution/', InstitutionCreateView.as_view(), name='create-institution'),
-    path('update-institution/<int:pk>/', InstitutionUpdateView.as_view(), name='update-institution'),
-    path('delete-institution/<int:pk>/', InstitutionDeleteView.as_view(), name='delete-institution'),
-    path('institution/<int:pk>/', InstitutionDetailView.as_view(), name='institution_detail'),
+    path('update-institution/<uuid:pk>/', InstitutionUpdateView.as_view(), name='update-institution'),
+    path('delete-institution/<uuid:pk>/', InstitutionDeleteView.as_view(), name='delete-institution'),
+    path('institution/<uuid:pk>/', InstitutionDetailView.as_view(), name='institution_detail'),
     path('search-institution/', search_view,name='search-institution'),
 ]
